5e320315267371327d196e1b8144739493db9cd3
Eugene Cobbah <cobbaheugene@gmail.com>
2025-08-31 10:10:12 +0000

    set up models and db

--- studybuddy/views.py
+++ studybuddy/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
+# room_list = [
+#     {'id':1, 'name':'Lets Learn Python!'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend Developers'},
+# ]
 
 def home(request):
-    return render (request, "home.html")
+    room_list = Room.objects.all()
+    context = {'rooms':room_list}
+    return render (request, "home.html", context)
 
-def rooms(request):
-    return render(request, 'rooms.html')
+def rooms(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'rooms':room}
+    return render(request, 'rooms.html', context)

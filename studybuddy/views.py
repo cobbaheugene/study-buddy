f52380c9db158733202cf8edb0b693a484eb35e3
Eugene Cobbah <cobbaheugene@gmail.com>
2025-08-31 07:19:46 +0000

    django template structure setup

--- studybuddy/views.py
+++ studybuddy/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render (request, "home.html")
 
 def rooms(request):
-    return HttpResponse("Your Available Rooms")
+    return render(request, 'rooms.html')
